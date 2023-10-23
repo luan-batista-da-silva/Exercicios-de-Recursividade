64b5934c9d86b334be6a595299eba9c62358c38d
unknown <Luan Batista da Silva>
2023-10-22 22:32:44 -0300

    Enunciado nos Próprios Códigos

--- L2Q1.c
+++ L2Q1.c
@@ -1,3 +1,39 @@
+/*
+Questão 1 -
+
+Por algum tempo houve polêmica quanto ao numeral 0 (zero) pertencer, ou não, aos números naturais,
+já que, habitualmente, não se inicia uma contagem pelo valor “zero”. Entretanto ele representa um conceito
+importante: a ausência de elementos num conjunto, seja ele abstrato ou concreto.
+A Matemática contemporânea representa o conjunto destes números por meio do símbolo N, incluindo o 0
+(zero). Para excluí-lo utiliza-se o asterisco como expoente: N∗, como feito no exemplo inicial desta questão.
+A partir deste conceito inicial a respeito dos números naturais, deseja-se que você escreva um programa,
+em C, para imprimir os n primeiros números naturais usando o conceito de recursividade, que os define da
+seguinte maneira:
+
+
+n0 = 0
+ni+1 = ni +1,i ∈ {0,1,2,...}
+
+Entrada
+A única linha da entrada contém um único natural n, indicando que se deseja imprimir os n primeiros
+números naturais, sendo que n ∈ N∗ e n ≤ 5000.
+
+Saída
+Seu programa deve imprimir uma única linha, contendo os n primeiros números naturais separados por
+um único espaço em branco entre eles.
+
+Exemplos:
+
+Entrada     |    Saída
+37          |    1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
+            |    26 27 28 29 30 31 32 33 34 35 36 37
+___________________________________________________________________________________
+Entrada     |   Saída
+50          |   1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
+            |   26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47
+            |   48 49 50
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
--- L2Q2.c
+++ L2Q2.c
@@ -1,3 +1,31 @@
+/*
+Questão 2 -
+
+Vamos a um problema envolvendo-a:
+Considere que seja dado um número n, n ∈ N∗. Usando o conceito de recursividade, elabore um programa em C para imprimir até o n-ésimo termo da “Série de Fibonacci”.
+Observação: Note que a contagem dos termos foi iniciada com o termo 0 (zero): f0 = 0.
+
+Entrada
+A única linha da entrada contém um número natural n, indicando a ordem máxima dos termos desejados
+da “Série de Fibonacci”. Sabe-se que 1 ≤ n ≤ 1000.
+
+Saída
+Seu programa deve imprimir uma única linha contendo até o n-ésimo termo da série, sempre separados
+por um único espaço em branco.
+
+Exemplos:
+
+Entrada        |     Saída
+0              |     0
+_________________________________________________
+Entrada        |     Saída
+1              |     0 1
+_________________________________________________
+Entrada        |     Saída
+8              |     0 1 1 2 3 5 8 13 21
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
